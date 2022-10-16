042a9506a2e39ae951e584646a38b5a429c6198a
meagherj <meagherj@greenhill.org>
2022-10-16 12:00:07 -0500

    fixed name search

--- nameSearch.py
+++ nameSearch.py
@@ -7,14 +7,17 @@ def findName(name):
   try:
     tryName = data[name]
   except:
-    print("Can't find name")
+    print("Can't find name:"+name)
+    return
   try:
     print(data[name]["2017"])
     print(data[name]["2018"])
     print(data[name]["2019"])
     print(data[name]["2020"])
     print(data[name]["2021"])
-
+  except:
+    print("Name: "+name+" does not exist in all years")
+    
 findName("Joseph")
 findName("Ethyl")
 findName("None")
